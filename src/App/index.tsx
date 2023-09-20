94498c071405008fb247dec5afba744362b8aef6
Wen Barbosa <wenblack012@gmail.com>
2023-09-20 15:06:23 -0300

    Created App folder

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,6 +1,6 @@
-import { Header } from './components/Header';
-import { Post, PostType } from './components/Post'
-import { Sidebar } from './components/Sidebar';
+import { Header } from '../components/Header';
+import { Post, PostType } from '../components/Post'
+import { Sidebar } from '../components/Sidebar';
 
 import styles from './App.module.css';
 
